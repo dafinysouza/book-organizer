a427d1cb6ca1405f05f8711a4a654c7e33750c7f
Dafiny Souza <dafinysouza@outlook.com>
2023-02-25 13:16:02 -0300

    chore: initial settings

---
+++ src/components/Banner/index.js
@@ -0,0 +1,14 @@
+import './style.css';
+
+function Banner() {
+    return (
+        <header className='banner'>
+            <img 
+                src="/images/banner.png" 
+                alt="Banner principal da página"
+            />
+        </header>
+    )
+}
+
+export default Banner;
